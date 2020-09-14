e52de75d9a1e329a7ffa245e14a48bbe4c3dc4d7
Hendy Nurfrianto <hendyn25@gmail.com>
2020-09-14 09:03:57 +0000

    Staircase

---
+++ Problem solve/staircase/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int
+    fmt.Scan(&a)
+
+    for i := 0; i < a; i++ {
+        for j := 0; j < a-i-1; j++ {
+            fmt.Print(" ")
+        }
+        for j := 0; j <= i; j++ {
+            fmt.Print("#")
+        }
+        fmt.Println()
+    }
+}
